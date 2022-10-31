5e6b5d0f3d2c6a821215d87c88cc072a871f03a8
Albert <user@WINDOWS-O7N9M38>
2022-10-31 21:05:56 +0400

    Add Register Models

---
+++ BankSystem/MyCredoBanking/Models/Request/LoginRequest.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyCredoBanking.Models.Request;
+
+public class LoginRequest
+{
+    [Required]
+    public string UserName { get; set; }
+    [Required]
+    [DataType(DataType.Password)]
+    public string Password { get; set; }
+    public bool RememberMe { get; set; }
+}
---
+++ BankSystem/MyCredoBanking/Models/Request/RegisterRequest.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyCredoBanking.Models.Request;
+
+public class RegisterRequest
+{
+    [Required]
+    public string FirstName { get; set; }
+
+    [Required]
+    public string LastName { get; set; }
+    [Required]
+    public string IdNumber { get; set; }
+    [Required]
+    [DataType(DataType.Date)]
+    public DateTime BirthDate { get; set; }
+
+    [Required]
+    [DataType(DataType.EmailAddress)]
+    public string Email { get; set; }
+
+
+    [Required]
+    [DataType(DataType.Password)]
+    public string Password { get; set; }
+    [Required]
+    [Compare("Password")]
+    [DataType(DataType.Password)]
+    public string ConfirmPassword { get; set; }
+}
