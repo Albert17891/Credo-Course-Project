fbc8aaa4a66640f2695608e39d88d624d6e2639b
Albert <user@WINDOWS-O7N9M38>
2022-10-31 22:29:07 +0400

    Add User Controler and view

---
+++ BankSystem/MyCredoBanking/Controllers/UserController.cs
@@ -0,0 +1,41 @@
+﻿using BankSystem.Domain.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyCredoBanking.Controllers;
+public class UserController : Controller
+{
+    private readonly UserManager<AppUser> _userManager;
+
+    public UserController(UserManager<AppUser> userManager)
+    {
+        _userManager = userManager;
+    }
+
+    [Route("Index")]
+    [HttpGet]
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    //public async Task<IActionResult> GetMyCards()
+    //{
+    //    //var cards=_
+    //}
+
+    //public async Task<IActionResult> GetMyAccounts()
+    //{
+
+    //}
+
+    //public async Task<IActionResult> SendToOther()
+    //{
+
+    //}
+
+    //public async Task<IActionResult> SendToMe()
+    //{
+
+    //}
+}
