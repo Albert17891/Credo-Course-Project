5fa3c29c4f9b3ce17ac0d8950bd42e04506dcac1
temo14 <77825356+temo14@users.noreply.github.com>
2022-10-31 19:49:00 +0400

    Add configurations

---
+++ BankSystem/BankSystem.PersistenceDB/Configuration/CreditCardConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using BankSystem.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankSystem.PersistenceDB.Configuration;
+
+public class CreditCardConfiguration : IEntityTypeConfiguration<CreditCard>
+{
+    public void Configure(EntityTypeBuilder<CreditCard> builder)
+    {
+        builder.Property(x => x.Pin).IsRequired();
+        builder.Property(x => x.Cvv).IsRequired();
+        builder.Property(x => x.CardNumber).IsRequired();
+        builder.Property(x => x.CardExpireDate).IsRequired();
+    }
+}
