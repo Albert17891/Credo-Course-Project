f40f01d93d382e2890b2ac87de34af2a08d8c8dd
Albert <user@WINDOWS-O7N9M38>
2022-11-01 22:52:37 +0400

    Comment Data Annotations

--- BankSystem/MyCredoBanking/Models/Request/CreditCardRequest.cs
+++ BankSystem/MyCredoBanking/Models/Request/CreditCardRequest.cs
@@ -7,15 +7,15 @@ public class CreditCardRequest
     public string UserId { get; set; }
     public int AccountId { get; set; }
     [Required]
-    [RegularExpression("^[0-9]{16}+$")]
+   // [RegularExpression("^[0-9]{16}+$")]
     public string CardNumber { get; set; }
     [Required]
     [DataType(DataType.Date)]
     public DateTime CardExpireDate { get; set; }
     [Required]
-    [RegularExpression("^[0-9]{3}+$")]
+  //  [RegularExpression("^[0-9]{3}+$")]
     public string Cvv { get; set; }
     [Required]
-    [RegularExpression("^[0-9]{4}+$")]
+   // [RegularExpression("^[0-9]{4}+$")]
     public string Pin { get; set; }
 }
