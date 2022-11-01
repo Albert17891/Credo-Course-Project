41a3fe9d99116785cdf8e91af4a4281bebc355e3
Albert <user@WINDOWS-O7N9M38>
2022-11-01 21:04:27 +0400

    add Tracsaction Controller and view

---
+++ BankSystem/MyCredoBanking/Controllers/TransactionController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyCredoBanking.Controllers;
+public class TransactionController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    [Route("SendToOther")]
+    [HttpGet]
+    public IActionResult SendToOther()
+    {
+        return View();
+    }
+
+    [Route("SendToMe")]
+    [HttpGet]
+    public async Task<IActionResult> SendToMe()
+    {
+        //var accounts = await GetMyAccounts();
+        //return View(accounts);
+        return Ok();
+    }
+
+
+    //public async Task<IActionResult> SendToMe()
+    //{
+
+    //}
+}
