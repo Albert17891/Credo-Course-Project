26439e38aedb55eb92a46d7e57ba79875ef9f3c1
temo14 <77825356+temo14@users.noreply.github.com>
2022-11-01 20:51:39 +0400

    add banking services.

---
+++ BankSystem/MyCredoBanking.Service/Abstractions/IOperatorService.cs
@@ -0,0 +1,9 @@
+﻿using MyCredoBanking.Service.Model;
+
+namespace MyCredoBanking.Service.Abstractions;
+
+public interface IOperatorService
+{
+    Task AddBankAccountForUser(UserAccountServiceModel userAccount);
+    Task AddCardForAccount(CreditCardServiceModel creditCard);
+}
---
+++ BankSystem/MyCredoBanking.Service/Model/CreditCardServiceModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCredoBanking.Service.Model
+{
+    public class CreditCardServiceModel
+    {
+        public int Id { get; set; }
+        public string UserId { get; set; }
+        public int AccountId { get; set; }
+        public string CardNumber { get; set; }
+        public DateTime CardExpireDate { get; set; }
+        public int Cvv { get; set; }
+        public int Pin { get; set; }
+    }
+}
