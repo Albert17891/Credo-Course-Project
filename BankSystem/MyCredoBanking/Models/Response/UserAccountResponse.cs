78e1522fc4fec6151148c8a734e042ea8e8ec1d2
temo14 <77825356+temo14@users.noreply.github.com>
2022-11-02 12:17:20 +0400

    add repository extension

--- BankSystem/MyCredoBanking/Models/Response/UserAccountResponse.cs
+++ BankSystem/MyCredoBanking/Models/Response/UserAccountResponse.cs
@@ -3,7 +3,6 @@
 public class UserAccountResponse
 {
     public int Id { get; set; }
-    public Account
     public string Iban { get; set; }
     public decimal Amount { get; set; }
     public string Currency { get; set; }
