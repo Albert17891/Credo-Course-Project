247514638b3aaac67c88e73b0f947a6727cb7935
temo14 <77825356+temo14@users.noreply.github.com>
2022-11-02 12:18:00 +0400

    change service models

--- BankSystem/MyCredoBanking.Service/Model/CreditCardServiceModel.cs
+++ BankSystem/MyCredoBanking.Service/Model/CreditCardServiceModel.cs
@@ -1,19 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace MyCredoBanking.Service.Model;
 
-namespace MyCredoBanking.Service.Model
+public class CreditCardServiceModel
 {
-    public class CreditCardServiceModel
-    {
-        public int Id { get; set; }
-        public string UserId { get; set; }
-        public int AccountId { get; set; }
-        public string CardNumber { get; set; }
-        public DateTime CardExpireDate { get; set; }
-        public int Cvv { get; set; }
-        public int Pin { get; set; }
-    }
+    public int Id { get; set; }
+    public string UserId { get; set; }
+    public int AccountId { get; set; }
+    public string CardNumber { get; set; }
+    public DateTime CardExpireDate { get; set; }
+    public string Cvv { get; set; }
+    public string Pin { get; set; }
 }
---
+++ BankSystem/MyCredoBanking.Service/Model/TransactionServiceModel.cs
@@ -0,0 +1,20 @@
+﻿using BankSystem.Domain.Models.Enum;
+
+namespace MyCredoBanking.Service.Model;
+
+public class TransactionServiceModel
+{
+    public string UserId { get; set; }
+
+    public string ReceiverUserId { get; set; }
+
+    public decimal TransferFee { get; set; }
+
+    public decimal TransferAmount { get; set; }
+
+    public DateTime TransactionDate { get; set; }
+
+    public decimal TransactionIncome { get; set; }
+
+    public TransactionType TransactionType { get; set; }
+}
