a0e64ec83e85e587bc36472e6ebc94fe8a00069f
Albert <user@WINDOWS-O7N9M38>
2022-11-03 18:21:17 +0400

    add ITransactionHelper

---
+++ BankSystem/MyCredoBanking.Service/Abstractions/ITransactionHelperService.cs
@@ -0,0 +1,13 @@
+﻿using BankSystem.Domain.Models;
+using Microsoft.AspNetCore.Identity;
+using MyCredoBanking.Service.Model;
+
+namespace MyCredoBanking.Service.Abstractions;
+public interface ITransactionHelperService
+{
+    Task<IList<UserAccountServiceModel>> GetAccounts(UserManager<AppUser> userManager, string userName);
+    Task<IList<UserAccountServiceModel>> GetAccounts(UserManager<AppUser> userManager, string userName,int firstAccountId);
+    Task<IList<UserAccountServiceModel>> GetAccoutsWithIdNumber(UserManager<AppUser> userManager,string IdNumber);
+
+
+}
