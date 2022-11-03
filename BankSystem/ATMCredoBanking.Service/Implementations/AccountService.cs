faf8db9ce0b46bbcbea424356989794c2d91b901
temo14 <77825356+temo14@users.noreply.github.com>
2022-11-03 19:36:24 +0400

    Add ATM Service.

---
+++ BankSystem/ATMCredoBanking.Service/Implementations/AccountService.cs
@@ -0,0 +1,25 @@
+﻿using AtmCredoBanking.Service.Abstractions;
+using BankSystem.DataAccess.Abstractions;
+using Microsoft.EntityFrameworkCore;
+
+namespace AtmCredoBanking.Service.Implementations;
+
+public class AccountService : IAccountService
+{
+    private readonly IContextWrapper _context;
+
+    public AccountService(IContextWrapper context)
+    {
+        _context = context;
+    }
+
+    public async Task<bool> CheckCard(string creditCardNumber, string pin)
+    {
+        var checkCard = await _context.cardRepository.Table.Where(x => x.CardNumber == creditCardNumber && x.Pin == pin).SingleOrDefaultAsync();
+
+        if (checkCard == null) 
+            return false;
+
+        return true;
+    }
+}
---
+++ BankSystem/ATMCredoBanking.Service/Implementations/CardService.cs
@@ -0,0 +1,58 @@
+﻿using AtmCredoBanking.Service.Abstractions;
+using BankSystem.DataAccess.Abstractions;
+using BankSystem.Domain.Models;
+using BankSystem.Domain.Models.Enum;
+
+namespace AtmCredoBanking.Service.Implementations;
+
+public class CardService : ICardService
+{
+    private readonly IContextWrapper _context;
+
+    public CardService(IContextWrapper context)
+    {
+        _context = context;
+    }
+
+    public async Task ChangePin(int cardId, string pin)
+    {
+        var card = await _context.cardRepository.GetByKeyAsync(cardId);
+
+        card.Pin = pin;
+
+        _context.Complete();
+    }
+
+    public async Task<decimal> ShowBalance(int acountId)
+    {
+        var account = await _context.userAccountRepository.GetByKeyAsync(acountId);
+
+        return account.Amount;
+
+    }
+
+    public async Task WithDraw(int acountId, decimal amount)
+    {
+        var account = await _context.userAccountRepository.GetByKeyAsync(acountId);
+
+        var fee = amount * 2 / 100;
+
+        var totalmoney = amount + fee;
+
+        if (account.Amount < totalmoney) throw new InvalidOperationException("Not enough money");
+
+        account.Amount -= totalmoney;
+
+        await _context.transactionRepository.AddEntityAsync(new Transactions
+        {
+            Currency = account.Currency,
+            TransferAmount = totalmoney,
+            TransferFee = fee,
+            UserId = account.UserId,
+            TransactionType = TransactionType.AtmTransaction,
+            TransactionDate = DateTime.Now
+        });
+
+        _context.Complete();
+    }
+}
