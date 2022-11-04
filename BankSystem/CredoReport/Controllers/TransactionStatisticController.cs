c91e05fc1cd1092addeae0d047124e3111b42726
Albert <user@WINDOWS-O7N9M38>
2022-11-04 10:41:35 +0400

    Create Methots in Controllers

---
+++ BankSystem/CredoReport/Controllers/TransactionStatisticController.cs
@@ -0,0 +1,67 @@
+﻿using CredoReport.Models.TransactionStastistic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CredoReport.Controllers;
+public class TransactionStatisticController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    [Route("QuantityTransaction")]
+    [HttpGet("quantity")]
+    public async Task<IActionResult> QuantityTransaction(int quantity)
+    {
+
+        if (quantity == 30)
+        {
+          
+            return View();
+        }
+        else if (quantity == 60)
+        {
+            return View();
+        }
+        else
+        {
+            return View();
+        }
+       
+    }
+   [Route("TransactionFeeQuantity")]
+    [HttpGet("Id")]
+    public async Task<IActionResult> TransactionFeeQuantity(int Id)
+    {
+      
+        if(Id==30)
+        {
+            var transFee = new TransactionFee() { QuantityGel = 120, QuantityUsd = 10, QuantityEuro = 30 };
+            return View(transFee);
+        }
+        else if(Id==60)
+        {
+            return View();
+        }
+        else
+        {
+            return View();
+        }
+        return Ok();
+    }
+
+    public async Task<IActionResult> AverageTransactionFee()
+    {
+        
+
+
+        return View();
+
+    }
+
+    public async Task<IActionResult> AtmMoneyQuantity()
+    {
+        return View();
+    }
+
+}
