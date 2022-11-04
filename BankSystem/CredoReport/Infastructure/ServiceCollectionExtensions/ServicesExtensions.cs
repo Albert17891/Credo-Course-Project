b3115461db9d8f4246837a9d32451475a1a4df39
Albert <user@WINDOWS-O7N9M38>
2022-11-04 17:36:06 +0400

    add Extensions for CredoReport projects

---
+++ BankSystem/CredoReport/Infastructure/ServiceCollectionExtensions/ServicesExtensions.cs
@@ -0,0 +1,17 @@
+﻿using CredoReport.Service.Abstractions;
+using CredoReport.Service.Service;
+
+namespace CredoReport.Infrastracture.ServiceCollectionExtensions;
+
+public static class ServicesExtensions
+
+{
+    public static IServiceCollection AddServicesExtension(this IServiceCollection services)
+    {
+        services.AddScoped<IUserStatisticService, UserStatisticService>();
+        services.AddScoped<ITransactionStatisticService, TransactionStatisticService>();
+
+        return services;
+    }
+}
+
