e750425bd9603680e5f37dfaaccc0b5b4b9e35f5
Albert <user@WINDOWS-O7N9M38>
2022-11-05 14:08:31 +0400

    add Data Validators

--- BankSystem/CredoATM/Models/ChangePin.cs
+++ BankSystem/CredoATM/Models/ChangePin.cs
@@ -1,6 +1,11 @@
-﻿namespace CredoATM.Models;
+﻿using CredoATM.Infrastracture.Resources;
+using System.ComponentModel.DataAnnotations;
+
+namespace CredoATM.Models;
 
 public class ChangePin
 {
+    [Required]
+    [RegularExpression("^[0-9]{4}$", ErrorMessage = ErrorMessage.Pin)]
     public string Pin { get; set; }
 }
