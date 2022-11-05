175eddd65245aaa2c8f2f3fab5d7a71ba33f0130
Albert <user@WINDOWS-O7N9M38>
2022-11-05 14:08:15 +0400

    add ErrorMessage in CredoAtm

---
+++ BankSystem/CredoATM/Infastructure/Resources/ErrorMessage.cs
@@ -0,0 +1,7 @@
+﻿namespace CredoATM.Infrastracture.Resources;
+
+public static class ErrorMessage
+{
+    public const string Pin = "ბარათის Pin უნდა იყოს რიცხვი და რაოდენობა უნდა იყოს 4";
+    public const string Amount = "შეიტანე სწორი რიცხვი ";
+}
