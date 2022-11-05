177a067f0b5e7bf75fe714cb6f669c9114af360e
Albert <user@WINDOWS-O7N9M38>
2022-11-05 14:10:32 +0400

    add new ErrorMessages

--- BankSystem/MyCredoBanking/Infrastracture/Resources/ErrorMessage.cs
+++ BankSystem/MyCredoBanking/Infrastracture/Resources/ErrorMessage.cs
@@ -6,4 +6,7 @@ public static class ErrorMessage
     public const string CardNumber = "ბარათის ნომერი უნდა იყოს რიცხვი და რაოდენობა უნდა იყოს 16";
     public const string Cvv = "ბარათის Cvv უნდა იყოს რიცხვი და რაოდენობა უნდა იყოს 3";
     public const string Pin = "ბარათის Pin უნდა იყოს რიცხვი და რაოდენობა უნდა იყოს 4";
+    public const string Iban = "თქვენს მიერ შეყვანილი ანგარიშის ნომერი არ არის სწორი";
+    public const string Email = "თქვენს მიერ შეყვანილი მაილი არ არის სწორი ";
+    public const string Amount = "შეიტანე სწორი რიცხვი ";
 }
