2411b80fb17fde2694610374701b167c18b4571a
Albert <user@WINDOWS-O7N9M38>
2022-11-05 12:33:07 +0400

    modify IUserRepository and implement it

--- BankSystem/BankSystem.DataAccess/Services/UserRepository.cs
+++ BankSystem/BankSystem.DataAccess/Services/UserRepository.cs
@@ -12,57 +12,10 @@ public class UserRepository : BaseRepository<AppUser>, IUserRepository
 	{
 		_context = context;
 	}
-	public async Task<int> GetUsersQuantity(int Id)
+	public async Task<int> GetUsersQuantity(double Id)
 	{
-		switch (Id)
-		{
-			case 30:
-				{
-					//For Month
-					return await _context.Users.Where(x => EF.Functions.DateDiffDay(x.RegisterTime, DateTime.Now) <= 30)
-								   .CountAsync();
-					break;
-				}
-			case 180:
-				{
-					//For This Year
-					var check = DateTime.Now.Subtract(new DateTime(DateTime.Now.Year, 1, 1)).TotalDays;
-
-					return await _context.Users.Where(x => EF.Functions
-											   .DateDiffDay(x.RegisterTime, DateTime.Now) <= check)
-											   .CountAsync();
-					break;
-				}
-			default:
-				{
-					//For One Year
-					return await _context.Users.Where(x => EF.Functions.DateDiffDay(x.RegisterTime, DateTime.Now) <= 365)
-								  .CountAsync();
-				}
-		}
-	}
-
-	public async Task<int> UsersRegisteredLast30Days()
-	{
-		var fromDate = DateTime.Today.AddDays(-30);
-
-		return await _context.Users.Where(x => x.RegisterTime >= fromDate)
+		return await _context.Users.Where(x => EF.Functions.DateDiffDay(x.RegisterTime, DateTime.Now) <= Id)
 					   .CountAsync();
-	}
 
-    public async Task<int> UsersRegisteredLastOneYear()
-    {
-		var fromDate = DateTime.Today.AddMonths(-12);
-
-		return await _context.Users.Where(x => x.RegisterTime >= fromDate)
-					   .CountAsync();
-	}
-
-    public async Task<int> UsersRegisteredthisYear()
-    {
-		var fromDate = new DateTime(DateTime.Now.Year, 1, 1);
-
-		return await _context.Users.Where(x => x.RegisterTime >= fromDate)
-					   .CountAsync();
 	}
 }
