59049e5b4fad66a0d4bed545aedc574c3b952aa8
temo14 <77825356+temo14@users.noreply.github.com>
2022-11-05 11:19:49 +0400

    add methods in Interfaces for Reports.

--- BankSystem/BankSystem.DataAccess/Abstractions/ITransactionRepository.cs
+++ BankSystem/BankSystem.DataAccess/Abstractions/ITransactionRepository.cs
@@ -1,12 +1,15 @@
 ﻿using BankSystem.Domain.Models;
+using BankSystem.Domain.Models.Extra;
 
 namespace BankSystem.DataAccess.Abstractions;
 
 public interface ITransactionRepository : IBaseRepository<Transactions>
 {
-    Task<int> GetTransactionsQuantity(int Id);  
+    Task<int> GetTransactionsQuantity(int days);
+
+    Task<TransferIncomes> GetTotalIncome(int days);
+
+    Task<TransferIncomes> GetAvgIncome();
 
     Task<decimal> GetAtmWithdrawTotal();
-
-
 }
--- BankSystem/BankSystem.DataAccess/Abstractions/IUserRepository.cs
+++ BankSystem/BankSystem.DataAccess/Abstractions/IUserRepository.cs
@@ -1,8 +1,14 @@
-﻿using BankSystem.Domain.Models;
+﻿namespace BankSystem.DataAccess.Abstractions;
 
-namespace BankSystem.DataAccess.Abstractions;
+using BankSystem.Domain.Models;
+
 public interface IUserRepository : IBaseRepository<AppUser>
 {
     Task<int> GetUsersQuantity(int Id);
 
+    Task<int> UsersRegisteredthisYear();
+
+    Task<int> UsersRegisteredLastOneYear();
+
+    Task<int> UsersRegisteredLast30Days();
 }
